df7ba7d840bbe93d5d09f3eaae5ec0c61c765384
douxing <douxing@zccp.com>
2020-04-26 10:07:27 +0800

    remove garbage code

--- include/idemix_issuer.h
+++ include/idemix_issuer.h
@@ -1,19 +1,6 @@
 #ifndef __IDEMIX_ISSUER_H__
 #define __IDEMIX_ISSUER_H__
 
-/*
-#include <gmp.h>
-#include "idemix_issuer_key.h"
-#include "idemix_primary_credential.h"
-
-void issue_primary_pre_credential
-(primary_pre_credential_t ppc, // OUT, known attributes already set in schema
- issuer_sk_t sk,
- issuer_pk_t pk,
- mpz_t U,   // from primary_pre_credential_prepare
- mpz_t n1); // from primary_pre_credential_prepare
-
-*/
 
 
 #endif // __IDEMIX_ISSUER_H__
